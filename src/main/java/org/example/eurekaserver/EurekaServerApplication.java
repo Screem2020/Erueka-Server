450e43ec4556c7516194f4d386b80a33980b2edf
Mihael <Churin_s@inbox.ru>
2026-07-13 19:17:23 +0200

    inital commit

---
+++ src/main/java/org/example/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,14 @@
+package org.example.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
